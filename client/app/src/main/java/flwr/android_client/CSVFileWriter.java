75306d9e92b281b35f63d4b2b61927192fa77855
fnaticIT <namanbalai06@gmail.com>
2023-02-17 10:23:08 +0530

    Federated + Stress

---
+++ client/app/src/main/java/flwr/android_client/CSVFileWriter.java
@@ -0,0 +1,34 @@
+package flwr.android_client;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CSVFileWriter {
+
+    private PrintWriter csvWriter;
+    private File file;
+
+    public CSVFileWriter(File file) {
+        this.file = file;
+
+    }
+
+    public void writeHeader(String data) {
+
+        try {
+            if (data != null) {
+
+                csvWriter = new PrintWriter(new FileWriter(file, true));
+                csvWriter.print(",");
+                csvWriter.print(data);
+                csvWriter.close();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
